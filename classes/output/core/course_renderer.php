23e10ffaffbc6f26c5ec0f65716c629a12a12a8c
Ismael FIlho <ismael.filho@stc.ma.gov.br>
2022-10-18 02:08:01 -0300

    Correção de erro nas demais páginas do Tem.a

--- classes/output/core/course_renderer.php
+++ classes/output/core/course_renderer.php
@@ -35,89 +35,27 @@ use core_course_list_element;
 use theme_uva\util\course_util;
 use core_course_category;
 
+//require_once($CFG->libdir . '/coursecatlib.php');
+
 class course_renderer extends \core_course_renderer
 {
 
 
-    /*private function coursecat_courses_carousel($chelper, $courses, $totalcount){
-        global $OUTPUT, $CFG;
+    public function get_summary_image($course) {
+        global $CFG, $OUTPUT;
 
-        $context = array();
-        $context["courses"] = array();
-        // $context["trails"] = array();
-        // $context["cartilhas"] = array();
+        foreach ($course->get_course_overviewfiles() as $file) {
+            if ($file->is_valid_image()) {
+                $url = moodle_url::make_file_url("$CFG->wwwroot/pluginfile.php",
+                    '/' . $file->get_contextid() . '/' . $file->get_component() . '/' .
+                    $file->get_filearea() . $file->get_filepath() . $file->get_filename(), !$file->is_valid_image());
 
-        foreach ($courses as $course){
-            //$relatedcontext = \context_course::instance($course->id);
-            //$course_data = extras::get_course_custom_fields(get_course($course->id)->id);
-
-            $current_course = new \stdClass();
-            $current_course->link = "$CFG->wwwroot/course/view.php?id=$course->id&page=introduction";
-            //$current_course->image = extras::get_course_summary_image($course);
-            $current_course->name = $chelper->get_course_formatted_name($course);
-            $current_course->description = $course->summary;
-            //$current_course->enrolledusers = count_enrolled_users($relatedcontext);
-            //$current_course->resources = extras::get_total_resources($course);
-            // $current_course->is_trail = $course->format == "trail_mtur";
-            // $current_course->is_course = $course->format == "saiteava" || $course->format == "classroom";
-            // $current_course->is_cartilha = $course->format == "singleactivity";
-            $current_course->coming_soon = $course->startdate > time();
-            $current_course->available = !boolval($current_course->coming_soon);
-
-           
-
-            
-            array_push ( $context["courses"], $current_course);
-            
+                return $url->out();
+            }
         }
-        $context["courses"] = array_slice($context["courses"], 0, 6);
-        // $context["trails"] = array_slice($context["trails"], 0, 6);
 
-        // $context["trails_count"] = sizeof($context["trails"]);
-        // $context["cartilhas_count"] = sizeof($context["cartilhas"]); // ! change to cartilhas
-        $context["output"] = $OUTPUT;
-
-        return $OUTPUT->render_from_template("theme_uva/frontpage_content", $context);
-    }*/
-
-    // protected function coursecat_coursebox_content(coursecat_helper $chelper, $course) {
-
-    //     global $OUTPUT;
-
-    //     if ($course instanceof stdClass) {
-    //         $course = new core_course_list_element($course);
-    //     }
-
-    //     $courseutil = new course($course);
-
-    //     $coursecontacts = $courseutil->get_course_contacts();
-
-    //     $courseenrolmenticons = $courseutil->get_enrolment_icons();
-    //     $courseenrolmenticons = !empty($courseenrolmenticons) ? $this->render_enrolment_icons($courseenrolmenticons) : false;
-
-    //     $courseprogress = $courseutil->get_progress();
-    //     $hasprogress = $courseprogress != null;
-
-    //     $data = [
-    //         'id' => $course->id,
-    //         'fullname' => $chelper->get_course_formatted_name($course),
-    //         'visible' => $course->visible,
-    //         'image' => $courseutil->get_summary_image(),
-    //         'summary' => $courseutil->get_summary($chelper),
-    //         'category' => $courseutil->get_category(),
-    //         'customfields' => $courseutil->get_custom_fields(),
-    //         'hasprogress' => $hasprogress,
-    //         'progress' => (int) $courseprogress,
-    //         'hasenrolmenticons' => $courseenrolmenticons != false,
-    //         'enrolmenticons' => $courseenrolmenticons,
-    //         'hascontacts' => !empty($coursecontacts),
-    //         'contacts' => $coursecontacts,
-    //         'teste' => 'hdiuahgsduiggdag',
-    //     ];
-        
-
-    //     echo($OUTPUT->render_from_template('theme_uva/frontpage_courses', $data));
-    // }
+        return $OUTPUT->get_generated_image_for_id($course->id);
+    }
 
     protected function coursecat_courses(coursecat_helper $chelper, $courses, $totalcount = null){
         
@@ -125,11 +63,8 @@ class course_renderer extends \core_course_renderer
 
         $context = array();
         $context["courses"] = array();
-        //$context["trails"] = array();
 
         foreach ($courses as $course){
-            $relatedcontext = \context_course::instance($course->id);
-            //$course_data = extras::get_course_custom_fields(get_course($course->id)->id);
 
             $cat = core_course_category::get($course->category, IGNORE_MISSING);
 
@@ -142,40 +77,42 @@ class course_renderer extends \core_course_renderer
 
             $current_course = new \stdClass();
             $current_course->link = "$CFG->wwwroot/course/view.php?id=$course->id&page=introduction";
-            //$current_course->image = course_util::get_summary_image_url();
+            $current_course->image = $this->get_summary_image($course);
             $current_course->name = $chelper->get_course_formatted_name($course);
-            $current_course->category = $cat->get_formatted_name();
-            //$current_course->description = $course->description;
-            //$current_course->enrolledusers = count_enrolled_users($relatedcontext);
-            //$current_course->resources = extras::get_total_resources($course);
-            //$current_course->is_trail = $course->format == "trail_saiteava";
-            //$current_course->is_course = $course->format == "saiteava" || $course->format == "classroom";
+            $current_course->category = $cat->get_formatted_name()/*$course->category*/;
+            $current_course->description = $course->summary;
 
-            if($course->startdate < 0 && $course->enddate > 0){
+            
+            /*if ($current_course->name === "Curso teste 2") {
+                echo('<pre>');
+                var_dump($course);
+                echo('</pre>');
+            }*/
+            
+
+            if($course->enddate > time()){
                 $current_course->status = "Online";
             }else{
                 $current_course->status = "Offline";
             }
 
             
-            array_push ( $context["courses"], $current_course);
+            array_push( $context["courses"], $current_course);
         }
         $context["courses"] = array_slice($context["courses"], 0, 3);
        
 
-        //$courses = $this->filter_subcourses($courses); aqui
-        //$totalcount = count($courses);
         
-        // frontpage available courses and trails
+        
         /*echo('<pre>');
-        var_dump($chelper);
+        var_dump($context["courses"][0]);
         echo('</pre>');*/
         
         if ($PAGE->pagetype == "site-index"){
             //$totalcount = count($courses);
             //return $this->coursecat_courses_carousel($chelper, $courses, $totalcount);
             //echo('teste');
-            return $OUTPUT->render_from_template("theme_uva/frontpage_courses", $context);
+            echo $OUTPUT->render_from_template("theme_uva/frontpage_courses", $context);
         }
     }
 }
